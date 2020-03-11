c68a490e6a73ae711b646174d65d76f1395a8a1c
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2020-03-11 10:53:16 +0900

    [create]add Animation

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
 
-class SideBar extends StatelessWidget {
+class SideBar extends StatefulWidget {
+  @override
+  _SideBarState createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<SideBar> {
+  AnimationController _animationController;
+  final bool isSidebarOpened = false;
+  final _animationDuration = const Duration(milliseconds: 500);
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this, duration: _animationDuration);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            color: Color(0xFF262AAA),
+    final screenWidth = MediaQuery.of(context).size.width;
+    return AnimatedPositioned(
+      duration: _animationDuration,
+      top: 0,
+      bottom: 0,
+      left: isSidebarOpened ? 0 : 0,
+      right: isSidebarOpened ? 0 : screenWidth - 45,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Color(0xFF262AAA),
+            ),
           ),
-        ),
-        Container(
-          width: 35,
-          height: 110,
-        )
-      ],
+          Align(
+            alignment: Alignment(0, -0.9),
+            child: Container(
+              width: 35,
+              height: 110,
+              color: Color(0xFF262AAA),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

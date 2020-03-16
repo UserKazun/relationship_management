0a8a45b7223b00872ec42301cac1c64931f0d1a1
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2020-03-16 13:31:12 +0900

    [update,create]update all file little bit.create new file(categories.dart) this file is make categories widget to HomeScreen.

--- lib/widgets/category_info.dart
+++ lib/widgets/category_info.dart
@@ -3,29 +3,40 @@ import 'package:flutter/material.dart';
 class CategoryInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 350,
-      height: 200,
-      color: Colors.grey.withOpacity(0.2),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 50, 
-                    width: 50,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        width: 350,
+        height: 150,
+        color: Colors.grey.withOpacity(0.2),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Text('All',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 32,
+                          color: Colors.blue
+                      ),
                     ),
-                  ),
-                  Text('15 people')
-                ],
-              )
+                    SizedBox(height: 70, width: 50),
+                    Text('15 people',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 32,
+                          color: Colors.blue
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }

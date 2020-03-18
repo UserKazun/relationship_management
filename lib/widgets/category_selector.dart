99b441bd3561fd00ad5dde5dd4e81d6e7a0fb355
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2020-03-19 00:03:32 +0900

    [update]changed design.

--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'category.dart';
+class CategorySelector extends StatefulWidget {
+  @override
+  _CategorySelectorState createState() => _CategorySelectorState();
+}
 
-class CategorySelector extends StatelessWidget {
+class _CategorySelectorState extends State<CategorySelector> {
+  int selectorIndex = 0;
+  final List<String> categories = ['All', 'Latest', 'Job', 'Private'];
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100.0,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          Category('all'),
-          Category('latest'),
-          Category('job'),
-          Category('matching'),
-          Category('private'),
-        ],
+      height: 90.0,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 30.0
+              ),
+              child: Text(categories[index]),
+            );
+          }
       ),
     );
   }

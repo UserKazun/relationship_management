013766d2e1a42b4a22db0fe6a4d677a453831d7c
kazunari.ueeda <kazuusjlo8@gmail.com>
2020-04-03 22:08:43 +0900

    update home_screen.dart

--- lib/src/ui/screens/home_screen.dart
+++ lib/src/ui/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:relationship_management/src/ui/widgets/app_drawer.dart';
+import 'package:relationship_management/src/ui/widgets/category_info.dart';
+import 'package:relationship_management/src/ui/widgets/category_selector.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -7,6 +9,12 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+      ),
+      body: Column(
+        children: <Widget>[
+          CategoryInfo(),
+          CategorySelector(),
+        ],
       ),
       drawer: AppDrawer(),
     );

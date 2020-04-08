09808715b4c8e9b6e38e2d3d8966935973dbe006
kazunari.ueeda <kazuusjlo8@gmail.com>
2020-04-08 22:56:03 +0900

    create new file

--- lib/src/ui/screens/item_create_screen.dart
+++ lib/src/ui/screens/item_create_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:relationship_management/src/ui/screens/create_form_screen.dart';
 
 class ItemCreateScreen extends StatelessWidget {
   static const String routeName = 'create_screen';
@@ -8,7 +9,7 @@ class ItemCreateScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('New Item')),
-      body: Text('test'),
+      body: CreateForm(),
     );
   }
 }

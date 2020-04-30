130ed50c90c88ba3792ddceef211b997548add87
kazunari.ueeda <kazuusjlo8@gmail.com>
2020-04-30 23:43:09 +0900

    Implementation of email address sign-in flow.

--- lib/src/ui/screens/signin_screen.dart
+++ lib/src/ui/screens/signin_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:relationship_management/src/services/auth.dart';
+
+import 'home_screen.dart';
 
 class SignInScreen extends StatefulWidget {
   static const String routeName = '/signIn_screen';
@@ -10,9 +13,64 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreenState extends State<SignInScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool loading = false;
+  String errorMessage = "";
 
-  final nameFocus = FocusNode();
-  final passwordFocus = FocusNode();
+  String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@'
+        r'((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)'
+        r'+[a-zA-Z]{2,}))$';
+
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter a valid Email!';
+    else
+      return null;
+  }
+
+  void _submitForm() async {
+    if (_formKey.currentState.validate()) {
+      try {
+        setState(() {
+          loading = true;
+        });
+        final user =
+        await Auth.signIn(_emailController.text, _passwordController.text);
+        if (user != null) {;
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen(user)),
+              ModalRoute.withName('/'));
+        }
+      } catch (error) {
+        setState(() {
+          loading = false;
+          switch (error.code) {
+            case 'ERROR_INVALID_EMAIL':
+              errorMessage = 'Your email address appears to be malformed.';
+              break;
+            case "ERROR_WRONG_PASSWORD":
+              errorMessage = "Incorrect password.";
+              break;
+            case "ERROR_USER_NOT_FOUND":
+              errorMessage = "User with this email doesn't exist.";
+              break;
+            case "ERROR_USER_DISABLED":
+              errorMessage = "User with this email has been disabled.";
+              break;
+            case "ERROR_TOO_MANY_REQUESTS":
+              errorMessage = "Too many requests. Try again later.";
+              break;
+            default:
+              errorMessage = "Something went wrong.";
+          }
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,11 +109,12 @@ class _SignInScreenState extends State<SignInScreen> {
                       children: <Widget>[
                         SizedBox(height: 5),
                         TextFormField(
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'mail address';
-                            }
-                            return null;
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          controller: _emailController,
+                          validator: (value) => validateEmail(value),
+                          onFieldSubmitted: (v) {
+                            FocusScope.of(context).nextFocus();
                           },
                           decoration: InputDecoration(
                             labelText: 'Email',
@@ -65,9 +124,11 @@ class _SignInScreenState extends State<SignInScreen> {
                         SizedBox(height: 10),
 
                         TextFormField(
+                          keyboardType: TextInputType.text,
+                          controller: _passwordController,
                           validator: (value) {
-                            if (value.isEmpty) {
-                              return 'password';
+                            if (value.length < 8) {
+                              return 'Password must be at least 8 characters';
                             }
                             return null;
                           },
@@ -75,6 +136,7 @@ class _SignInScreenState extends State<SignInScreen> {
                             labelText: 'Password',
                             prefixIcon: Icon(Icons.lock),
                           ),
+                          obscureText: true,
                         ),
                         SizedBox(height: 10),
 
@@ -83,7 +145,6 @@ class _SignInScreenState extends State<SignInScreen> {
                             'Forget your password?'
                           ),
                           onPressed: () {
-
                           },
                         ),
 
@@ -93,7 +154,7 @@ class _SignInScreenState extends State<SignInScreen> {
                               'Sign In'
                           ),
                           onPressed: () {
-
+                            _submitForm();
                           },
                         ),
                       ],
